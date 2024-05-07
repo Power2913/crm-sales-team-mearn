5e167f5bba10fb51aa7e98721d3ca88ee6da552f
Nitesh Chauhan <72297535+Nitesh121-maker@users.noreply.github.com>
2024-05-07 16:00:13 +0530

    Committed on 07 at 16:00

--- client/src/frontend/Notification.jsx
+++ client/src/frontend/Notification.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import '../css/notification.css';
 const Notification = ({ notification }) => {
-    console.log(notification)
+    // console.log(notification)
   return (
     <div className="notification-container">
         <h2 className="notification-heading">Notifications</h2>
         <ul className="notification-list">
-        {Array.isArray(notification) &&
-            notification.map((notification, index) => (
-            <li key={index} className="notification-item">
-                <div className="notification-content">
-                <span className="notification-icon">🔔</span>
-                <span className="notification-text">Update status of {notification.clientName}</span>
-                </div>
-            </li>
-            ))}
+        {Array.isArray(notification) && notification.map((notification, index) => {
+            const lastSeenTime = new Date(notification.last_seen);
+            const currentTime = new Date();
+            const twentyFourHoursAgo = new Date(currentTime.getTime() - (24 * 60 * 60 * 1000)); // Subtract 24 hours from the current time
+
+            // Check if last_seen time is 24 hours or more in the past
+            const is24HoursOrMore = lastSeenTime <= twentyFourHoursAgo;
+
+            if (is24HoursOrMore) {
+                return (
+                    <li key={index} className="notification-item">
+                        <div className="notification-content">
+                            <span className="notification-icon">🔔</span>
+                            <span className="notification-text">Update status of {notification.clientName}</span>
+                        </div>
+                    </li>
+                );
+            } else {
+                return null; 
+            }
+        })}
+
         </ul>
     </div>
   );
